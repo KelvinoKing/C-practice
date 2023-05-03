4b1b2127f78b9eb242bf70847601817bd48e19b1
Kelvino8860 <kelvinnjoroge2001@students.uonbi.ac.ke>
2023-05-03 13:50:14 +0300

    Cards programme

--- cards.c
+++ cards.c
@@ -18,6 +18,10 @@ int main()
 	{
 		val = 10;
 	}
+	else if (card_name[0] == 'J')
+	{
+		val = 10;
+	}
 	else if (card_name[0] == 'A')
 	{
 		val = 11;
@@ -27,6 +31,15 @@ int main()
 		val = atoi(card_name);
 	}
 
-	printf("The card value is: %i\n", val);
+	/* Check if the value is 3 to 6 */
+	if (val >= 3 && val <= 6)
+	{
+		puts("Count has gone up");
+	}
+	/* Otherwise check if the card was 10,J,Q or K */
+	else if (val == 10)
+	{
+		puts("Count has gone down");
+	}
 	return 0;
 }
