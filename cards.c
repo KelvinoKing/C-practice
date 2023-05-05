6d920a109234599e7ec6f49401e1f8bd339e09e0
Kelvino8860 <kelvinnjoroge2001@students.uonbi.ac.ke>
2023-05-05 14:26:06 +0300

    Cards programme

--- cards.c
+++ cards.c
@@ -6,22 +6,44 @@
 int main()
 {
 	char card_name[3];
-	puts("Enter the card_name: ");
-	scanf("%2s", card_name);
-	int val = 0;
+	int count = 0;
 
-	switch(card_name[0])
+	while (card_name[0] != 'X')
 	{
-		case 'K':
-		case 'Q':
-		case 'J':
-			val = 10;
-			break;
-		case 'A':
-			val = 11;
-			break;
-		default:
-			val = atoi(card_name);
+    puts("Enter the card_name: ");
+	  scanf("%2s", card_name);
+	  int val = 0;
+
+	  switch(card_name[0])
+	  {
+		  case 'K':
+		  case 'Q':
+		  case 'J':
+			  val = 10;
+			  break;
+		  case 'A':
+			  val = 11;
+			  break;
+			case 'X':
+			  continue;
+		  default:
+			  val = atoi(card_name);
+
+			if (val < 1 || val > 10)
+			{
+				puts("Sorry!!! unrecognized card");
+				continue;
+			}
+	  }
+
+		if (val > 2 && val < 7)
+			count++;
+		else if (val == 10)
+		{
+			count--;
+		}
+
+		printf("Current count: %i\n", count);
 	}
 
 	return 0;
